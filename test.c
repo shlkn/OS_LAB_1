52c780fbe5bc0fbc1b2f7b9ba9560085bfc47595
iliya shilkin <pro100iliyash@yandex.ru>
2021-03-13 09:37:06 -0800

    fixed bags

--- test.c
+++ test.c
@@ -15,6 +15,7 @@ struct file_descriptor
 	unsigned int id; // file id
 	unsigned int pr_id; // parents id
 };
+
 void handle_file(int arch, int *file_cnt, char *file_name, int pr_id)
 {
 	struct stat fl_stat;
@@ -37,12 +38,13 @@ void handle_file(int arch, int *file_cnt, char *file_name, int pr_id)
 	write(arch, &fl_desc, sizeof(struct file_descriptor));
 	return;
 }
-void handle_dir(int arch, int *file_cnt, char *dir_name, int pr_id)
+
+void handle_dir(int arch, int *file_cnt, char *path, char *dir_name, int pr_id)
 {
 	DIR *dp;
 	struct dirent *cr_dir;
 	struct file_descriptor dir_desc;
-	dp = opendir(dir_name);
+	dp = opendir(path);
 	if(dp == NULL) 
 	{
 		printf("Error can`t open dir\n");
@@ -52,6 +54,7 @@ void handle_dir(int arch, int *file_cnt, char *dir_name, int pr_id)
 	dir_desc.is_reg = 0; // type - dir
 	dir_desc.fl_sz = 0; // writing size of dir - 0
 	strcpy(dir_desc.fl_name, dir_name); // writing file name
+	printf("dir id - %d\n", *file_cnt);
 	dir_desc.id = *file_cnt; // writing file id
 	dir_desc.pr_id = cr_pr_id; // writing parents id
 	printf("         home dir     \n");
@@ -77,28 +80,85 @@ void handle_dir(int arch, int *file_cnt, char *dir_name, int pr_id)
 		if(cr_dir->d_type == 8) // если просто файл
 			handle_file(arch, file_cnt, cr_dir->d_name, dir_desc.id);
 		else if(cr_dir->d_type == 4) // если директория
-		{
-			char buff[100];
-			strcpy(buff, dir_name);
-			//printf("dir - %s\n", cr_dir->d_name);
-			strcat(buff, "/");
-			strcat(buff, cr_dir->d_name);
-			printf("%s\n", buff);
-			handle_dir(arch, file_cnt, buff, dir_desc.id);
+		{	
+			char full_path[100];
+			strcpy(full_path, path);
+			strcat(full_path, "/");
+			strcat(full_path, cr_dir->d_name);
+			printf("dir - %s\n", path);
+			//printf("%d\n", dir_desc.id);
+			handle_dir(arch, file_cnt, full_path, cr_dir->d_name, dir_desc.id);
 		}
 	}
 }
-
-int main(int argc, char* argv[])
+void copy_data()
 {
+
+}
+void pack(char *file_names_arr[], int arg_cnt, int start_index)
+{
+	struct stat fl_stat;
 	int arch;
 	int file_cnt = 0; //count of struct
 	if((arch = open("my_arch", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)) == -1)
 		printf("Error, can`t create archive file \n");
 	write(arch, &file_cnt, sizeof(int));
-	handle_dir(arch, &file_cnt, argv[1], file_cnt);
-	printf("%d\n", file_cnt);
+	for(int i = start_index; i < arg_cnt; i++)
+	{
+		lstat(file_names_arr[i], &fl_stat);
+		if(S_ISREG(fl_stat.st_mode))
+		{
+			handle_file(arch, &file_cnt, file_names_arr[i], 0);
+		}
+		else if(S_ISDIR(fl_stat.st_mode))
+		{
+			char path[100];
+			strcpy(path, file_names_arr[i]);
+			handle_dir(arch, &file_cnt, path, file_names_arr[i], -1);//составили и записали дискрипторы для дерикторий и файлов
+		}
+	} 
+	// необходимо придумать алгоритм запаковки файлов
+	//записали дискриптор для каждого файла
+	/*char buff[1024];
+	int file;
+	printf("arg_cnt - %d start_index - %d", arg_cnt, start_index);
+	for(int i = start_index; i < arg_cnt; i++)
+	{
+		int nread;
+		if((file = open(file_names_arr[i], O_RDONLY, S_IRUSR | S_IWUSR)) == -1)
+			printf("Error, can`t open destination file\n");
+		while((nread = read(file, &buff, sizeof(buff))) > 0)
+		{
+			write(arch, buff, nread);
+			printf("%s", buff);
+		}
+		close(file);
+	}*/
 	lseek(arch, SEEK_SET, 0);
 	write(arch, &file_cnt, sizeof(int));
 	close(arch);
+}
+void unpack()
+{
+
+}
+int main(int argc, char* argv[])
+{
+	int opt = getopt(argc, argv, "pu:");
+	switch(opt)
+	{
+		case 'p':
+		{
+			pack(argv, argc, optind);
+			printf("Done\n");
+			break;
+		}
+		case 'u':
+		{
+			unpack(optarg);
+			break;
+		}
+		break;
+	}
+	return 1;
 }
